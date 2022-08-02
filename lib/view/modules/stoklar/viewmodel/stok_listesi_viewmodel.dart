e9bb9ee6ee300f855819bbf8d756abdfe66dd7c9
ertugrulcakici <ertugrul.cakici@std.hku.edu.tr>
2022-08-02 15:52:18 +0300

    stok listesi arama eklendi

--- lib/view/modules/stoklar/viewmodel/stok_listesi_viewmodel.dart
+++ lib/view/modules/stoklar/viewmodel/stok_listesi_viewmodel.dart
@@ -1,22 +1,51 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:nodemobile/core/services/database/database_helper.dart';
 
 class StokListesiViewModel extends ChangeNotifier {
   StokListesiViewModel();
 
+  List filtered = [];
   late LocaleDatatable stokListesi;
+  late TextEditingController textEditingController;
 
   bool setupDone = false;
 
-  Future init() async {
+  Future init(TextEditingController controller) async {
+    textEditingController = controller;
     await DatabaseHelper.instance.stokManager.updateStocks();
     setupDone = true;
     await fillStocks();
+    filtered.addAll(stokListesi.rowsAsJson);
+    log(filtered.first.toString());
     notifyListeners();
   }
 
   Future fillStocks() async {
     stokListesi = await DatabaseHelper.instance.stokManager.getNonVariants();
+    notifyListeners();
+  }
+
+  onChanged(String value) {
+    log(value);
+    filtered.clear();
+    if (value.isEmpty) {
+      filtered.addAll(stokListesi.rowsAsJson);
+    } else {
+      log("not empty");
+      for (Map<String, dynamic> element in stokListesi.rowsAsJson) {
+        String searchString =
+            "${element["Name"].toString()} ${element["Aciklama"].toString()} ${element["Barcode"].toString()} ${element["Miktar"].toString()} ${element["UnitCode"].toString()}"
+                .toLowerCase();
+        log("geldi");
+        if (value
+            .split(" ")
+            .every((part) => searchString.contains(part.toLowerCase()))) {
+          filtered.add(element);
+        }
+      }
+    }
     notifyListeners();
   }
 }
